478fa193fcceb8a6b9662ed940b32b6c698cbe65
Clément Niot <clement.niot@gmail.com>
2025-03-25 12:41:00 +0100

    [CHORE] logout: ajout endpoint pour la déconnexion

--- base/views/auth.py
+++ base/views/auth.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
@@ -18,3 +20,8 @@ class AuthViewSet(ViewSet):
         responsable = register_user_data.save()
         serializer = UserSerializer(responsable)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['get'], url_path='logout', serializer_class=None, url_name="logout", basename="auth-logout")
+    def logout(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
