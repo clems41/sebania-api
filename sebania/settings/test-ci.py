acd650b45387de9a5d8d65c072d36708097bd053
Clément Niot <clement.niot@gmail.com>
2025-03-25 09:39:04 +0100

    [CHORE] ci: ajout du fichier settings spécifique aux tests dans la CI

---
+++ sebania/settings/test-ci.py
@@ -0,0 +1,41 @@
+from .base import *
+
+SECRET_KEY = 'my_secret_test_key'
+
+FIXTURE_DIRS = [BASE_DIR / 'fixtures']
+
+DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
