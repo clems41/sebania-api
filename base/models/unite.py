5c7cd322a364da6d6d0ab13dda7a946a413d0203
Clément Niot <clement.niot@gmail.com>
2025-04-28 10:36:52 +0200

    [FEAT] unites: ajout champ pour la compatibilité entre les unités et l'activité de récolte

--- base/models/unite.py
+++ base/models/unite.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Unite(models.Model):
     nom = models.CharField(max_length=30)
-    label = models.CharField(max_length=2)
+    recolte_compatible = models.BooleanField(default=False)
