12570e22c600a4363cef8db724f28610d08a91dc
Clément Niot <clement.niot@gmail.com>
2025-05-06 18:26:49 +0200

    [REFACTOR] analyze: utilisation de l'agent Mistral au lieu de CrewAI

--- thomas_ai/tasks/analyze.py
+++ thomas_ai/tasks/analyze.py
@@ -2,13 +2,13 @@ import json
 from datetime import datetime
 
 from background_task import background
+from django.conf import settings
 from django.utils import timezone
+from mistralai import Mistral
 
-from base.models import Culture, Activite, Unite
 from base.models.vocal import Vocal
 from sebania.exceptions.custom_exception import CustomException
 from sebania.exceptions.error_code import ErrorCode
-from thomas_ai.crewai.crew import AnalyzerCrew
 from thomas_ai.tasks.extract import extract
 
 
@@ -17,42 +17,34 @@ def analyze(vocal_id: int):
     # Récupération du vocal sans l'audio qui a déjà été traité
     vocal = Vocal.objects.defer('audio').get(id=vocal_id)
 
-    # Analyse avec CrewAI
+    # Analyse avec Mistral Agent
     start_time = datetime.now()
-    inputs = _get_inputs()
-    inputs['transcription'] = vocal.transcription
-    crew = AnalyzerCrew()
-    outputs = crew.crew().kickoff(inputs=inputs)
+    mistral_api_key = settings.MISTRAL_API_KEY
+
+    client = Mistral(api_key=mistral_api_key)
+
+    chat_response = client.agents.complete(
+        agent_id="ag:76bf0d16:20250506:untitled-agent:9faabefa",
+        messages=[
+            {
+                "role": "user",
+                "content": vocal.transcription,
+            },
+        ],
+    )
 
     # Check output
-    if len(outputs.tasks_output) != crew.get_nb_agents():
-        raise CustomException(ErrorCode.IA_CREWAI_OUTPUT_LEN_INCORRECTE, actual=len(outputs.tasks_output), expected=crew.get_nb_agents())
-    output = outputs.tasks_output[crew.get_nb_agents() - 1].raw
+    if len(chat_response.choices) == 0:
+        raise CustomException(ErrorCode.IA_CREWAI_OUTPUT_LEN_INCORRECTE, actual=0, expected=1)
+    output = chat_response.choices[0].message.content
 
     # Mise à jour du vocal avec l'analyse
     end_time = datetime.now()
     duration = end_time - start_time
     vocal.transcription_to_output_duration = duration
     vocal.analyzed_at = timezone.now()
-    vocal.output = _cleanup_output(output)
+    vocal.output = json.loads(output)
     vocal.save()
 
     # Envoi dans la queue suivante pour l'extraction des tâches à partir du JSON généré
     extract(vocal_id=vocal_id)
-
-def _get_inputs():
-    cultures = Culture.objects.all()
-    activites = Activite.objects.all()
-    unites = Unite.objects.all()
-    culture_noms = [c.nom for c in cultures]
-    activite_noms = [a.nom for a in activites]
-    unite_noms = [u.nom for u in unites]
-    return {
-        'cultures': ','.join(culture_noms),
-        'activites': ','.join(activite_noms),
-        'unites': ','.join(unite_noms)
-    }
-
-def _cleanup_output(output):
-    output = output.replace('```json\n', '').replace('\n```', '')
-    return json.loads(output)
