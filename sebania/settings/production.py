85249f08c591d71aeb99db3b3d6024092a4afc0f
Clément Niot <clement.niot@gmail.com>
2025-05-18 13:58:40 +0200

    [CHORE] jwt: augmentation durée de vie token

--- sebania/settings/production.py
+++ sebania/settings/production.py
@@ -1,4 +1,6 @@
 # SECURITY WARNING: don't run with debug turned on in production!
+from datetime import timedelta
+
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1:8000', ]
 CACHES = {
@@ -6,6 +8,16 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
     }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=100),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME_LATE_USER': timedelta(days=30),
+    'UPDATE_LAST_LOGIN': True,
 }
 
 
