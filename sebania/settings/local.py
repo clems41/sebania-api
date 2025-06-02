35308b7d6beadbdb318aaee93b4d80e396f9f878
Clément Niot <clement.niot@gmail.com>
2025-06-02 17:35:39 +0200

    [CHORE] CORS: correction problème CORS pour le dev en local

--- sebania/settings/local.py
+++ sebania/settings/local.py
@@ -8,6 +8,16 @@ INSTALLED_APPS += [
 ]
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME_LATE_USER': timedelta(days=30),
+    'UPDATE_LAST_LOGIN': True,
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
