69ee36ae54fb7255b4cd170f80c41399619c96df
Clément Niot <clement.niot@gmail.com>
2025-07-21 13:36:04 +0200

    [CHORE] cache: suppression du memcache

--- sebania/settings/production.py
+++ sebania/settings/production.py
@@ -3,12 +3,6 @@ from datetime import timedelta
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1:8000', ]
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
