817f1d0840b87d4076eaf62a0d40b471d7918437
Clément Niot <clement.niot@gmail.com>
2025-07-22 11:57:21 +0200

    [CHORE] cors: specify cors allowed origins

--- sebania/settings/production.py
+++ sebania/settings/production.py
@@ -1,8 +1,6 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 from .base import *
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ORIGIN_WHITELIST").split(",")
 
 DEBUG = False
