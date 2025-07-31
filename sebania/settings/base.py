0bedcf53b71c39ce19c42c99604b4082e9d476ba
Clément Niot <clement.niot@gmail.com>
2025-07-31 12:13:14 +0200

    [CHORE] mistral: séparation des workspaces

--- sebania/settings/base.py
+++ sebania/settings/base.py
@@ -204,6 +204,6 @@ WHISPER_MODEL_DIRECTORY = os.getenv('WHISPER_MODEL_DIRECTORY', "/tmp/whisper_mod
 LEMONFOX_API_KEY = os.getenv('LEMONFOX_API_KEY')
 MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
 MISTRAL_AGENTS = {
-    "taches": "ag:76bf0d16:20250515:untitled-agent:832efb79",
-    "parcelles": "ag:76bf0d16:20250528:agent-parcelle:411ba7b9",
+    "taches": os.getenv('MISTRAL_AGENT_TACHE_ID'),
+    "parcelles": os.getenv('MISTRAL_AGENT_PARCELLE_ID'),
 }
